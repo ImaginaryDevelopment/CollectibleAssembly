ab1b05859012140524358848ae3b5793a2a4fd3a
DimperioB <DimperioB@duvalschools.org>
2024-10-01 16:32:16 -0400

    csharp works

--- src/CHost/Program.cs
+++ src/CHost/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Linq;
+
+var path = @"C:\dev\CollectibleAssembly\src\CImpl\bin\Debug\net8.0\CImpl.dll";
+
+using var ms = new System.IO.MemoryStream(System.IO.File.ReadAllBytes(path));
+var context = new System.Runtime.Loader.AssemblyLoadContext("CImpl", collectible: true);
+var asm = context.LoadFromStream(ms);
+
+var t = asm.ExportedTypes.First(et => et.GetInterface(nameof(Schema.IAmImplementation)) != null);
+
+Schema.IAmImplementation value = Activator.CreateInstance(t) as Schema.IAmImplementation;
+
+Console.WriteLine(value.GetHello());
+
+// won't actually do anything if references survive
+context.Unload();
+
+value.GetHello();
+
+Console.WriteLine("Hello post unload?");
--- src/FHost/Program.fs
+++ src/FHost/Program.fs
@@ -1,6 +1,13 @@
 ﻿// https://www.strathweb.com/2019/01/collectible-assemblies-in-net-core-3-0/#:~:text=Collectible%20assemblies%20are%20implemented%20in%20.NET%20Core%203.0
 // https://jeremybytes.blogspot.com/2020/01/dynamically-loading-types-in-net-core.html#:~:text=Create%20a%20custom%20assembly%20load%20context.%20Switch%20from%20using
+// didn't load: https://jonathancrozier.com/blog/how-to-dynamically-load-different-versions-of-an-assembly-in-the-same-dot-net-application#:~:text=We%20can%20use%20the%20LoadFromAssemblyPath%20method%20on%20the
+// https://siderite.dev/blog/dynamically-loading-types-from-assembly.html/#:~:text=.SelectMany(assembly%20=%3E%20assembly.GetTypes().Where(t%20=%3E
 
+module Option =
+    let ofEmptyList =
+        function
+        | [] -> None
+        | v -> Some v
 
 let mutable myCollectible = None
 // let mutable myImpl = None
@@ -20,11 +27,14 @@ let findDll start (name: string) =
     |> Seq.tryFind (fun fn -> fn.Contains name)
 // |> fun v -> name, v
 
-let loadAndRunAssembly =
-    function
-    | CImpl -> "CImpl", findDll @"..\..\src\CImpl\bin\" "CImpl.dll"
-    | FImpl -> "FImpl", findDll @"..\..\src\FImpl\bin\" "FImpl.dll"
-    >> fun (name, path) ->
+let loadAndRunAssembly it =
+    match it, myCollectible with
+    // don't reload the same context
+    | CImpl, Some(CImpl, context, _) -> None
+    | FImpl, Some(FImpl, context, _) -> None
+    | CImpl, _ -> findDll @"..\..\src\CImpl\bin\" "CImpl.dll"
+    | FImpl, _ -> findDll @"..\..\src\FImpl\bin\" "FImpl.dll"
+    |> fun path ->
         match path with
         | None -> failwith "Unable to find path"
         | Some path ->
@@ -33,11 +43,41 @@ let loadAndRunAssembly =
             printfn "Loading: '%s'" fullPath
 
             use ms = new System.IO.MemoryStream(System.IO.File.ReadAllBytes(fullPath))
-            let context = new System.Runtime.Loader.AssemblyLoadContext(name, true)
-            myCollectible <- Some context
-            let asm = context.Assemblies
+            let context = new System.Runtime.Loader.AssemblyLoadContext(string it, true)
+            let asm = context.LoadFromStream(ms)
+            myCollectible <- Some(it, context, asm)
+            context.Assemblies |> Seq.length |> printfn "Loaded %i asm(s)"
 
-            oldContext |> Option.iter (fun c -> c.Unload())
+            let asm =
+                context.Assemblies
+                |> Seq.choose (fun asm ->
+                    printfn "Asm location is '%s'" asm.Location
+
+                    [
+
+                      asm.DefinedTypes
+                      |> Seq.choose (fun et ->
+                          printfn "Searching type: %s" et.FullName
+                          et.GetInterface(nameof Schema.IAmImplementation, true) |> Option.ofObj)
+                      |> List.ofSeq
+                      |> List.tryHead
+
+                      asm.ExportedTypes
+                      |> Seq.choose (fun et ->
+                          printfn "Searching type: %s" et.FullName
+                          et.GetInterface(nameof Schema.IAmImplementation, true) |> Option.ofObj)
+                      |> List.ofSeq
+                      |> List.tryHead ]
+                    |> List.choose id
+                    |> List.tryHead)
+                |> List.ofSeq
+                |> List.tryHead
+
+            match asm with
+            | None -> eprintfn "No impl found in asm: '%s'" path
+            | Some asm -> printfn "Found impl!"
+
+            oldContext |> Option.iter (fun (_, c, _) -> c.Unload())
 
 
 
